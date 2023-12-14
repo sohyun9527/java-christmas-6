3b4bed82622e3f85e9710c4d8a698d532e33c432
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 13:39:12 +0900

    feat(InputView): 입력 기능

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,34 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String REQUEST_VISIT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String REQUEST_MENU_AND_COUNT = "주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public String readVisitDay() {
+        System.out.println(REQUEST_VISIT_DAY);
+        String input = getUserInput();
+        validateEmptyLine(input);
+
+        return input;
+    }
+
+    public String readOrder() {
+        System.out.println(REQUEST_MENU_AND_COUNT);
+        String input = getUserInput();
+        validateEmptyLine(input);
+
+        return input;
+    }
+
+    private void validateEmptyLine(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 아무것도 입력하지 않았습니다.");
+        }
+    }
+
+    private String getUserInput() {
+        return Console.readLine();
+    }
+}

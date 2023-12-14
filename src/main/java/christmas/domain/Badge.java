a02d9a856cff61d41b0e3f87a16f12f6e686324c
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 16:06:24 +0900

    feat(Badge): 없음 추가

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -4,7 +4,7 @@ public enum Badge {
     SANTA("산타", 20_000),
     TREE("트리", 10_000),
     STAR("별", 5_000),
-    NONE("없음");
+    NONE("없음", 0);
 
     private final String badge;
     private final int amount;

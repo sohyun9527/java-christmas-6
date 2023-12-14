6d77f662dd6fe58b05463e5e34fc97d575352852
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 15:29:21 +0900

    feat(DiscountType): 할인 방식들 열거형으로 저장

---
+++ src/main/java/christmas/domain/DiscountType.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public enum DiscountType {
+    CHRISTMAS("크리스마스 디데이 할인"),
+    WEEKEND("주말 할인"),
+    WEEKDAY("평일 할인"),
+    SPECIAL("특별 할인"),
+    PROMOTION("증정 메뉴");
+
+    private final String type;
+
+    DiscountType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}

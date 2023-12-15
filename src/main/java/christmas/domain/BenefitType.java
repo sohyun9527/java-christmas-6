421fcba67687c2b248092a9c8850f5b233d8838c
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 19:14:20 +0900

    feat(BenefitType): 혜택 타입들 열거형 저장

---
+++ src/main/java/christmas/domain/BenefitType.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+public enum BenefitType {
+    CHRISTMAS("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인"),
+    PROMOTION("증정 이벤트");
+
+    private final String type;
+
+    BenefitType(String type) {
+        this.type = type;
+    }
+}

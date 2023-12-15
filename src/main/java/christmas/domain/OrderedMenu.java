2d6de226d53fcc8c5e4cfadcb3f9ce284d5b2d1d
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 18:40:36 +0900

    feat(OrderedMenu): 메뉴-개수 하나에 대한 검증

---
+++ src/main/java/christmas/domain/OrderedMenu.java
@@ -0,0 +1,60 @@
+package christmas.domain;
+
+import christmas.domain.exception.OrderException;
+import java.util.List;
+
+public class OrderedMenu {
+    private static final int MINIMUM_COUNT = 1;
+    private static final int VALID_SIZE = 2;
+    private static final String NAME_AND_COUNT_DELIMITER = "-";
+    private final Menu menu;
+    private final int count;
+
+    public OrderedMenu(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    public static OrderedMenu from(String input) {
+        List<String> nameAndCount = List.of(input.split(NAME_AND_COUNT_DELIMITER, -1));
+        validate(nameAndCount);
+        Menu menu = Menu.of(nameAndCount.get(0));
+        int count = Integer.parseInt(nameAndCount.get(1));
+
+        return new OrderedMenu(menu, count);
+    }
+
+    public int getPrice() {
+        return menu.getPrice() * count;
+    }
+
+    private static void validate(List<String> nameAndCount) {
+        String count = nameAndCount.get(1);
+        validateInputSize(nameAndCount);
+        validateCount(count);
+    }
+
+    private static void validateCount(String count) {
+        validateOnlyDigit(count);
+        validateMinimumCount(count);
+    }
+
+    private static void validateMinimumCount(String count) {
+        if (Integer.parseInt(count) < MINIMUM_COUNT) {
+            throw new OrderException();
+        }
+    }
+
+    private static void validateOnlyDigit(String count) {
+        if (!count.chars().allMatch(Character::isDigit)) {
+            throw new OrderException();
+        }
+    }
+
+
+    private static void validateInputSize(List<String> nameAndCount) {
+        if (nameAndCount.size() != VALID_SIZE) {
+            throw new OrderException();
+        }
+    }
+}

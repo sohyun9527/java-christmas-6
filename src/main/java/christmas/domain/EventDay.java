241fa6cb586eded39eea06418f1f520f150b676c
sohyun95277 <sohyun95277@gmail.com>
2023-12-15 18:23:47 +0900

    feat(EventDay): 날짜를 입력받아 이벤트를 판별하는 기능

--- src/main/java/christmas/domain/EventDay.java
+++ src/main/java/christmas/domain/EventDay.java
@@ -1,21 +1,44 @@
 package christmas.domain;
 
 import christmas.domain.exception.EventDayException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class EventDay {
     private static final int FIRST_DAY = 1;
     private static final int LAST_DAY = 31;
+    private static final int CHRISTMAS = 25;
+    private static final int YEAR = 2023;
     private final int day;
+    private final DayOfWeek dayOfWeek;
 
-    public EventDay(int day) {
+    public EventDay(int day, DayOfWeek dayOfWeek) {
         this.day = day;
+        this.dayOfWeek = dayOfWeek;
     }
 
-    // 숫자가 아닌가?
-    // 2023.12 내의 수인가?
     public static EventDay from(String input) {
         validate(input);
-        return new EventDay(Integer.parseInt(input));
+        int inputDay = Integer.parseInt(input);
+        return new EventDay(inputDay, getDayOfWeek(inputDay));
+    }
+
+    public boolean isBeforeChristmas() {
+        return day <= CHRISTMAS;
+    }
+
+    public boolean isWeekEnd() {
+        return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
+    }
+
+    public boolean isSpecial() {
+        return dayOfWeek == DayOfWeek.SATURDAY || day == CHRISTMAS;
+    }
+
+    private static DayOfWeek getDayOfWeek(int day) {
+        LocalDate date = LocalDate.of(YEAR, Month.DECEMBER, day);
+        return date.getDayOfWeek();
     }
 
     private static void validate(String input) {
@@ -34,5 +57,9 @@ public class EventDay {
         if (day < FIRST_DAY || day > LAST_DAY) {
             throw new EventDayException();
         }
+    }
+
+    public int getDay() {
+        return day;
     }
 }
